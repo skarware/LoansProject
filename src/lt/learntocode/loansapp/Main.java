86cd29261fe1e23e8c289181cc90b133d0fcb1c5
Martynas Skaringa <skarware@gmail.com>
2019-12-03 15:38:46 +0200

    minor change for testing

--- src/lt/learntocode/loansapp/Main.java
+++ src/lt/learntocode/loansapp/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-//     // Testing Mode for TESTING PURPOSES ONLY
+        // Testing Mode for TESTING PURPOSES ONLY
 //        loanServices.testingMode();
 
         // To start loan calculator
